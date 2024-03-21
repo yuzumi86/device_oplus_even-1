32ef82a1abc99577a23b3284f506e989fb3a0b16
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-03-21 14:14:46 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -51,7 +51,7 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 # Audio
 PRODUCT_PACKAGES += \
- #   audio.a2dp.default
+    audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/configs/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
