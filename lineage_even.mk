5c4f82df5b54732d6c245382aec7459e825013ea
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-05-06 13:19:37 +0700

    even: delete non ab devices and change fingerprint

--- lineage_even.mk
+++ lineage_even.mk
@@ -20,7 +20,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/even/device.mk)
@@ -37,11 +36,11 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Build info
-BUILD_FINGERPRINT := "google/coral/coral:12/SP2A.220505.002/8353555:user/release-keys"
+BUILD_FINGERPRINT := "realme/RMX3191T2/RMX3191:11/RP1A.200720.011/1647614838132:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=even \
     PRODUCT_NAME=even \
-    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1607914664672-release-keys"
+    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1647614838132-release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
