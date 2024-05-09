8003dd78e0231c192c6298d907a1c1dbfb28fa81
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-05-09 07:20:35 +0700

    even: banana bring ups and add stock fingerprint

--- banana_even.mk
+++ banana_even.mk
@@ -15,29 +15,21 @@
 #
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-RISING_BUILDTYPE := UNOFFICIAL
-RISING_MAINTAINER := ismasrull
-TARGET_BUILD_APERTURE_CAMERA := true
-WITH_GMS := true
-TARGET_CORE_GMS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_CORE_GMS_EXTRAS := true
-TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/banana/config/common_full_phone.mk)
+
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/even/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_even
+PRODUCT_NAME := banana_even
 PRODUCT_DEVICE := even
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := realme C25 Series
+PRODUCT_MODEL := realme C25
 PRODUCT_MANUFACTURER := realme
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_GAPPS_ARCH := arm64
@@ -45,12 +37,17 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_USE_PIXEL_CHARGER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# banana ify
+BANANA_MAINTAINER := ismasrull
+BANANA_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := true
+
 # Build info
-BUILD_FINGERPRINT := "google/coral/coral:12/SP2A.220505.002/8353555:user/release-keys"
+BUILD_FINGERPRINT := "realme/RMX3191T2/RMX3191:11/RP1A.200720.011/1647614838132:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=even \
     PRODUCT_NAME=even \
-    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1607914664672-release-keys"
+    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1647614838132-release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
