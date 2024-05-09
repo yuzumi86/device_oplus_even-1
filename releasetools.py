2de9bb4b4c56034c5da1871c4cc4939bc3aef8e1
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-05-09 07:25:03 +0700

    even: add vbmeta_vendor

--- releasetools.py
+++ releasetools.py
@@ -41,4 +41,5 @@ def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+  AddImage(info, "vbmeta_vendor.img", "/dev/block/bootdevice/by-name/vbmeta_vendor")
   return
