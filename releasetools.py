437c8a74d96e4ea0f196ee1e0c9f2700a41486d8
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-05-13 05:02:08 +0700

    even: initial files

--- releasetools.py
+++ releasetools.py
@@ -2,26 +2,10 @@
 # Copyright (C) 2019 The Mokee Open Source Project
 # Copyright (C) 2019 The LineageOS Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
 import common
 import re
-
-def FullOTA_InstallBegin(info):
-  data = info.input_zip.read("RADIO/dynamic-remove-oppo")
-  common.ZipWriteStr(info.output_zip, "dynamic-remove-oppo", data)
-  info.script.AppendExtra('update_dynamic_partitions(package_extract_file("dynamic-remove-oppo"));')
-  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
